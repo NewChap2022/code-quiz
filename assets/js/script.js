42a0d6d1d6cd250c4f05502a9cc71cf33c9f42f8
NewChap2022 <irisyangshu@gmail.com>
2022-05-19 23:34:32 -0400

    start and jump to first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
-var generateQuiz = function(questions, quizcontainer, result) {
-    
+var startBtn = document.querySelector("#start-btn");
+var intro = document.querySelector("#intro");
+var main = document.querySelector("main");
+var quizContent = [
+    {
+        question: "lorem jeleljt dielf fjlejti fjoelet jfieo jeoe fiejfj fjiefj fjielfj fjieljf eilmc jioejf jioejf fjei",
+        options: ["jieoegoep", "jioetpmf", "jeiwpjpw", "jeipemtg"],
+        answer: 0
+    },
+];
+
+var quizCreate = function () {
+    var quiz = document.createElement("div");
+    quiz.id = "quiz";
+    main.appendChild(quiz);
+
+    var questionContainer = document.createElement("div");
+    questionContainer.className = "question-container";
+    questionContainer.id = "question";
+    questionContainer.innerHTML = quizContent[0].question;
+    quiz.appendChild(questionContainer);
+
+    var optionContainer = document.createElement("div");
+    optionContainer.className = "option-container";
+    quiz.appendChild(optionContainer);
+
+    for (var i = 0; i < 4; i++) {
+        var btn = document.createElement("button");
+        btn.setAttribute("type", "button");
+        btn.className = "option";
+        btn.id = "opt-" + i+1 + "";
+        optionContainer.appendChild(btn);
+        btn.innerText = quizContent[0].options[i];
+    }
+
+    var resultContainer = document.createElement("div");
+    resultContainer.id = "result";
+    resultContainer.innerHTML = "Do you know the answer?";
+    quiz.appendChild(resultContainer);
 }
+
+startBtn.onclick = function () {
+    intro.style.display = "none";
+    quizCreate()
+};
