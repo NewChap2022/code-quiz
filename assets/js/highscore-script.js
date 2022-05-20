8a211b31ba5f6ab26790908457dff8db2ab40b7f
NewChap2022 <irisyangshu@gmail.com>
2022-05-20 13:53:06 -0400

    add highscore function

---
+++ assets/js/highscore-script.js
@@ -0,0 +1,17 @@
+var loadScore = function() {
+    var savedScore = localStorage.getItem("savedScore");
+    if (!savedScore) {
+        return false;
+    };
+
+    savedScore = JSON.parse(savedScore);
+    savedScore = savedScore.sort(function(a, b) {return b.score - a.score});
+
+    // reference from https://stackoverflow.com/questions/70298582/how-to-make-a-leaderboard-ranking-scores-javascript
+    for(var i = 0; i < savedScore.length; i++){
+        tableContent = "<tr><td>" + (i + 1) + "</td><td>" + savedScore[i].name + "</td><td>" + savedScore[i].score + "</td></tr>";
+        document.querySelector("#highscore-table").innerHTML += tableContent;
+      };
+}
+
+loadScore();
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var startBtn = document.querySelector("#start-btn");
 var intro = document.querySelector("#intro");
 var main = document.querySelector("main");
 var questionIndexCount = 0;
+var savedScore = JSON.parse(localStorage.getItem("savedScore"));
+
 var quizContent = [
     {
         question: "lorem jeleljt dielf fjlejti fjoelet jfieo jeoe fiejfj fjiefj fjielfj fjieljf eilmc jioejf jioejf fjei",
@@ -35,7 +37,7 @@ var quizCreate = function () {
         var btn = document.createElement("button");
         btn.setAttribute("type", "button");
         btn.className = "option";
-        btn.id = "opt-" + (i+1) + "";
+        btn.id = "opt-" + (i + 1) + "";
         optionContainer.appendChild(btn);
         btn.innerText = quizContent[questionIndexCount].options[i];
     }
@@ -53,7 +55,7 @@ startBtn.onclick = function () {
 
 var answerQuiz = function (event) {
     var selectAnswer = event.target;
-    if (selectAnswer.matches(".option")) {    
+    if (selectAnswer.matches(".option")) {
         var indexAnswer = quizContent[questionIndexCount].answer;
         var answerContent = quizContent[questionIndexCount].options[indexAnswer];
         var chosenOption = selectAnswer.innerHTML;
@@ -64,7 +66,7 @@ var answerQuiz = function (event) {
             result.innerHTML = "Wrong Answer";
         };
 
-        setTimeout(makeQuestion, 3000);
+        setTimeout(makeQuestion, 2000);
     }
 };
 
@@ -73,12 +75,31 @@ var makeQuestion = function () {
     var result = document.querySelector("#result");
     result.innerHTML = "Do you know the answer?"
     questionIndexCount++;
-    var questionContainer = document.querySelector("#question");
-    questionContainer.innerHTML = quizContent[questionIndexCount].question;
-    for (var i = 0; i < 4; i++) {
-        var btn = document.querySelector("#opt-" + (i+1) + "");
-        btn.innerText = quizContent[questionIndexCount].options[i];
-    };
+    if (questionIndexCount <= quizContent.length - 1) {
+        var questionContainer = document.querySelector("#question");
+        questionContainer.innerHTML = quizContent[questionIndexCount].question;
+        for (var i = 0; i < 4; i++) {
+            var btn = document.querySelector("#opt-" + (i + 1) + "");
+            btn.innerText = quizContent[questionIndexCount].options[i];
+        };
+    } else {
+        var quiz = document.querySelector("#quiz");
+        quiz.style.display = "none";
+        alert("Congratulations! You have answered all the questions and let's check out how you do. Your score is ")
+        saveScore();
+    }
 }
+
+  var saveScore = function () {
+            var name = prompt("What's your name?");
+            var score = "40";
+            var playerScore = {name: name, score: score};
+            if (!savedScore) {
+                savedScore = [];
+            };
+            savedScore.push(playerScore);
+            localStorage.setItem("savedScore", JSON.stringify(savedScore));
+            window.open("highscore.html", "_self")
+        }
 
 main.addEventListener("click", answerQuiz);
